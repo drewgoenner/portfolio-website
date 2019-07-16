3b0b31500c9c9c7d251b7db8196baa97a527297b
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-07-16 16:03:13 -0500

    completed project, about page links to personal info, changed js script for page load time

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -28,7 +28,7 @@
 		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 100);
+			}, 500);
 		});
 
 	// Fix: Flexbox min-height bug on IE.
@@ -397,5 +397,7 @@
 					$window.on('load', function() {
 						$main._show(location.hash.substr(1), true);
 					});
+
+					
 
 })(jQuery);
